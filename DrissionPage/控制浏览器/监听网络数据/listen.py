e969e430ecbc90d46f20c0e1978f4691c49e6d92
hook-404 <xping8804@gmail.com>
2025-08-15 08:13:16 +0800

    fix

--- DrissionPage/控制浏览器/监听网络数据/listen.py
+++ DrissionPage/控制浏览器/监听网络数据/listen.py
@@ -1,11 +1,27 @@
+# from DrissionPage import Chromium
+
+# # 等待并获取
+# tab = Chromium().latest_tab
+# tab.get("https://gitee.com/explore/all")
+
+# tab.listen.start("gitee.com/explore")
+# for _ in range(5):
+# 	tab("@rel=next").click()
+# 	res = tab.listen.wait()
+# 	print(res.url)
+
+
+# 实时获取
 from DrissionPage import Chromium
 
-
 tab = Chromium().latest_tab
-tab.get("https://gitee.com/explore/all")
+tab.listen.start('gitee.com/explore')  # 开始监听，指定获取包含该文本的数据包
+tab.get('https://gitee.com/explore/all')  # 访问网址
 
-tab.listen.start("gitee.com/explore")
-for _ in range(5):
-	tab("@rel=next").click()
-	res = tab.listen.wait()
-	print(res.url)
+i = 0
+for packet in tab.listen.steps():
+    print(packet.url)  # 打印数据包url
+    tab('@rel=next').click()  # 点击下一页
+    i += 1
+    if i == 5:
+        break
